3236cc1737b7c45d8eacd118ed593664c16e4814
Arlando A. Ifrene <arlandoifrene@gmail.com>
2026-03-22 01:36:14 -0300

    Feat: Reorganized in n-layered architecture...

---
+++ BLL/BLLProduct.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+using DAL;
+
+namespace BLL
+{
+    public class BLLProduct : IRepository<Product>
+    {
+        DALProduct dalProduct = new DALProduct();
+
+        public void Add(Product item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+           return dalProduct.GetAll();
+        }
+
+        public void Update(Product item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Purchase_Serialize_Deserialize_Sample/Program.cs
+++ Purchase_Serialize_Deserialize_Sample/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using BE;
+using BLL;
 
 namespace Purchase_Serialize_Deserialize_Sample
 {
@@ -15,7 +16,8 @@ namespace Purchase_Serialize_Deserialize_Sample
         {
             try
             {
-                
+                var bllProduct = new BLLProduct();
+
                 var customer = new Customer
                 {
                     IDNumber = 12345678,
@@ -26,21 +28,7 @@ namespace Purchase_Serialize_Deserialize_Sample
                     FullAddress = "123 Main St, Anytown, USA"
                 };
 
-                var products = new List<Product>
-                {
-                  new Product { Name = "Wireless Mouse", Price = 29.99m },
-                  new Product { Name = "Mechanical Keyboard", Price = 89.95m },
-                  new Product { Name = "USB-C Hub", Price = 39.50m },
-                  new Product { Name = "27-inch Monitor", Price = 249.99m },
-                  new Product { Name = "Laptop Stand", Price = 45.00m },
-                  new Product { Name = "Noise-Cancelling Headphones", Price = 199.99m },
-                  new Product { Name = "Webcam 1080p", Price = 59.99m },
-                  new Product { Name = "External SSD 1TB", Price = 129.99m },
-                  new Product { Name = "Portable Charger 10000mAh", Price = 34.95m },
-                  new Product { Name = "Smartphone Case", Price = 19.99m },
-                  new Product { Name = "Bluetooth Speaker", Price = 79.99m },
-                  new Product { Name = "Ethernet Cable (10ft)", Price = 9.99m }
-                };
+                var products = bllProduct.GetAll();
 
                 var purchase = new Purchase(customer, products);
 
